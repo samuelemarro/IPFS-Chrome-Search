ea30be52532060ccd964497d6d2e155fd72de1f8
Samuele Marro <marrosamuele@gmail.com>
2021-03-26 22:25:38 +0100

    Added defaultAction.

--- background.js
+++ background.js
@@ -1,18 +1,25 @@
-storedSuggestion = null
-gateway = 'https://gateway.ipfs.io/ipfs/'
+var storedSuggestion = null;
+// IPFS Gateway/API, will be updated after reading from storage
+var gateway = "https://gateway.ipfs.io/ipfs/";
 
-function updateGateway() {
+// Default action
+var defaultAction = "search";
+
+function updateOptions() {
     chrome.storage.sync.get({
-        gateway: 'https://gateway.ipfs.io/ipfs/'
-    }, function(items) {
+        defaultAction: "search",
+        gateway: "https://gateway.ipfs.io/ipfs/"
+    }, function (items) {
+        defaultAction = items.defaultAction;
         gateway = items.gateway;
+        console.log("defaultAction: " + defaultAction);
     });
 }
 
 function goToPage(hash) {
     chrome.tabs.update({
         "url": gateway + hash
-    }, function() {
+    }, function () {
         console.log("Bug Page is open");
     });
 }
@@ -20,58 +27,99 @@ function goToPage(hash) {
 function standardSearch(text) {
     chrome.tabs.update({
         "url": "https://ipfs-search.com/#/search?search=" + text
-    }, function() {
+    }, function () {
         console.log("Bug Page is open");
     });
 
 }
 
 function querySearch(text) {
-    fetch("https://api.ipfs-search.com/v1/search?q=" + encodeURI(text) + "&type=any&page=1")
-        .then(function(response) {
+    fetch("https://api.ipfs-search.com/v1/search?q=" + encodeURI(text) + "&type=any&page=1", {
+            mode: 'cors'
+        })
+        .then(function (response) {
             return response.json();
         })
-        .then(function(myJson) {
+        .then(function (myJson) {
             parseSuggestions(myJson, text);
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log("Error: " + error);
         });
+}
+
+function isDefault(text) {
+    return text.charAt(text.length - 1) !== " ";
+}
+
+function getTrailingText(text) {
+    let trailingText = "<dim> | ";
+
+    let useDefault = isDefault(text);
+
+    if (defaultAction === "go") {
+        if (useDefault) {
+            trailingText += "Jump to Page (Space + Enter to search)";
+        } else {
+            trailingText += "Search on IPFS";
+        }
+    } else {
+        // Includes both "search" and any unrecognized value
+
+        if (useDefault) {
+            trailingText += "Search on IPFS (Space + Enter to jump to page)";
+        } else {
+            trailingText += "Jump to Page";
+        }
+
+        if (defaultAction !== "search") {
+            console.log(`Unrecognized action ${defaultAction}.`);
+        }
+    }
+
+    trailingText += "</dim>";
+
+    return trailingText;
 }
 
 function parseSuggestions(jsonResponse, originalText) {
     // Set first suggestion as the default suggestion
     console.log(jsonResponse);
-    hit = jsonResponse.hits[0]
+    let hit = jsonResponse.hits[0];
     storedSuggestion = hit.hash;
 
-    if (originalText.charAt(originalText.length - 1) === " ") {
-        trailingText = "<dim> | Jump to Page</dim>"
-    } else {
-        trailingText = "<dim> | Search on IPFS (Space + Enter to jump to page)</dim>"
-    }
+    let trailingText = getTrailingText(originalText);
 
+    // Chrome only allows one suggestion from an extension
     chrome.omnibox.setDefaultSuggestion({
         description: (hit.title + trailingText)
     });
 
 }
 
-// This event is fired each time the user updates the text in the omnibox,
-// as long as the extension's keyword mode is still active.
+// Fired when the user changes the content of the omnibox
 chrome.omnibox.onInputChanged.addListener(
-    function(text, suggest) {
-        updateGateway();
-        console.log('inputChanged: ' + text);
+    function (text, suggest) {
+        updateOptions();
+        console.log("inputChanged: " + text);
         querySearch(text);
     });
 
-// This event is fired with the user accepts the input in the omnibox.
+// Fired when the user accepts the suggestion
 chrome.omnibox.onInputEntered.addListener(
-    function(text) {
-        if (text.charAt(text.length - 1) === " ") {
-            goToPage(storedSuggestion, text);
+    function (text) {
+        if (isDefault(text)) {
+            if (defaultAction === "go") {
+                goToPage(storedSuggestion, text);
+            } else {
+                standardSearch(text);
+            }
+
         } else {
-            standardSearch(text);
+            if (defaultAction === "go") {
+                standardSearch(text);
+            } else {
+                goToPage(storedSuggestion, text);
+            }
         }
     });
--- options.js
+++ options.js
@@ -1,14 +1,16 @@
 // Saves options to chrome.storage
 function save_options() {
-    var gateway = document.getElementById('gateway').value;
+    let defaultAction = document.getElementById("defaultAction").value;
+    let gateway = document.getElementById("gateway").value;
     chrome.storage.sync.set({
+      defaultAction: defaultAction,
       gateway: gateway
     }, function() {
       // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
+      let status = document.getElementById("status");
+      status.textContent = "Options saved.";
       setTimeout(function() {
-        status.textContent = '';
+        status.textContent = "";
       }, 750);
     });
   }
@@ -17,12 +19,13 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    gateway: 'https://gateway.ipfs.io/ipfs/'
+    defaultAction: "search",
+    gateway: "https://gateway.ipfs.io/ipfs/"
   }, function(items) {
-    console.log('Setting gateway to ' + items.gateway)
-    document.getElementById('gateway').value = items.gateway;
+    console.log("Setting gateway to " + items.gateway)
+    document.getElementById("gateway").value = items.gateway;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click",
     save_options);
