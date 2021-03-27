89db3fb6de6a2e46e6a896178cb156fec785c093
Samuele Marro <marrosamuele@gmail.com>
2021-03-27 22:32:51 +0100

    Added more error handling.

--- background.js
+++ background.js
@@ -10,6 +10,9 @@ var defaultAction = "search";
 // Cache size
 var cacheSize = 100;
 
+// Latest omnibox text
+var omniboxText = "";
+
 // Read from the options storage
 function updateOptions() {
     chrome.storage.sync.get({
@@ -65,7 +68,12 @@ function querySearch(text) {
                 console.log("Error: " + error);
             });
     } else {
-        setDefaultSuggestion(cachedSuggestion.title, text);
+        // If the user hasn't already inserted another query, provide
+        // the suggestion
+        if (omniboxText == text) {
+            setDefaultSuggestion(cachedSuggestion.title, text);
+        }
+        
     }
 }
 
@@ -79,12 +87,47 @@ function getCachedSuggestion(text) {
     return result.value;
 }
 
+// Sets the default suggestion for the Omnibox
 function setDefaultSuggestion(title, originalText) {
-    let trailingText = getTrailingText(originalText);
+    let suggestionText = "";
+    let useDefault = isDefault(originalText);
+    let cachedSuggestion = getCachedSuggestion(originalText);
+    let foundResult = (cachedSuggestion != null) && (title != null);
+
+    if (foundResult) {
+        suggestionText += title;
+    }
+    suggestionText += "<dim>";
+    
+    if (foundResult) {
+        suggestionText += " | "
+    }
+
+    if (defaultAction === "go") {
+        if (!useDefault || !foundResult) {
+            suggestionText += "Search on IPFS";
+        } else {
+            suggestionText += "Jump to Page (Space + Enter to search)";
+        }
+    } else {
+        // Includes both "search" and any unrecognized value
+
+        if (useDefault || !foundResult) {
+            suggestionText += "Search on IPFS (Space + Enter to jump to page)";
+        } else {
+            suggestionText += "Jump to Page";
+        }
+
+        if (defaultAction !== "search") {
+            console.log(`Unrecognized action ${defaultAction}.`);
+        }
+    }
+
+    suggestionText += "</dim>";
 
     // Chrome only allows one suggestion from an extension
     chrome.omnibox.setDefaultSuggestion({
-        description: (title + trailingText)
+        description: (suggestionText)
     });
 }
 
@@ -93,64 +136,37 @@ function isDefault(text) {
     return text.charAt(text.length - 1) !== " ";
 }
 
-// Get the text that appears after the suggestion
-function getTrailingText(text) {
-    let trailingText = "<dim> | ";
-
-    let useDefault = isDefault(text);
-    let cachedSuggestion = getCachedSuggestion(text);
-
-    if (defaultAction === "go") {
-        if (!useDefault || cachedSuggestion == null) {
-            trailingText += "Search on IPFS";
-        } else {
-            trailingText += "Jump to Page (Space + Enter to search)";
-        }
-    } else {
-        // Includes both "search" and any unrecognized value
-
-        if (useDefault || cachedSuggestion == null) {
-            trailingText += "Search on IPFS (Space + Enter to jump to page)";
-        } else {
-            trailingText += "Jump to Page";
-        }
-
-        if (defaultAction !== "search") {
-            console.log(`Unrecognized action ${defaultAction}.`);
-        }
-    }
-
-    trailingText += "</dim>";
-
-    return trailingText;
-}
-
 // Handle the suggestion received from the IPFS API
 function parseSuggestions(jsonResponse, originalText) {
     // Set first suggestion as the default suggestion
     console.log("Received suggestion response: " + jsonResponse);
     let hit = jsonResponse.hits[0];
 
-    if (storedSuggestions.length > cacheSize) {
-        storedSuggestions.shift();
-    }
-    storedSuggestions.push({
-        key: originalText,
-        value: {
-            title: hit.title,
-            hash: hit.hash
+    if (hit != null) {
+        if (storedSuggestions.length > cacheSize) {
+            storedSuggestions.shift();
         }
-    });
+        storedSuggestions.push({
+            key: originalText,
+            value: {
+                title: hit.title,
+                hash: hit.hash
+            }
+        });
+    }
 
-    setDefaultSuggestion(hit.title, originalText);
-
+    // If the user hasn't already inserted another query, provide
+    // the suggestion
+    if (omniboxText == originalText) {
+        setDefaultSuggestion(hit == null ? null : hit.title, originalText);
+    }
 }
 
 // Fired when the user changes the content of the omnibox
 chrome.omnibox.onInputChanged.addListener(
     function (text, suggest) {
         updateOptions();
-        console.log("inputChanged: " + text);
+        omniboxText = text;
         querySearch(text);
     });
 
