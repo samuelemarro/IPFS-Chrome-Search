bfedc0de8d8839a7afe00f4ae4e459b8b2e59c0a
Samuele Marro <marrosamuele@gmail.com>
2021-03-27 22:12:02 +0100

    Added suggestion caching.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-// Contains the suggestion returned by IPFS Search
-var storedSuggestion = null;
+// Contains the suggestions returned by IPFS Search
+var storedSuggestions = [];
 
 // IPFS Gateway/API, will be updated after reading from storage
 var gateway = "https://gateway.ipfs.io/ipfs/";
@@ -7,51 +7,85 @@ var gateway = "https://gateway.ipfs.io/ipfs/";
 // Default action
 var defaultAction = "search";
 
+// Cache size
+var cacheSize = 100;
+
 // Read from the options storage
 function updateOptions() {
     chrome.storage.sync.get({
         defaultAction: "search",
-        gateway: "https://gateway.ipfs.io/ipfs/"
+        gateway: "https://gateway.ipfs.io/ipfs/",
+        cacheSize: 100
     }, function (items) {
         defaultAction = items.defaultAction;
         gateway = items.gateway;
-        console.log("defaultAction: " + defaultAction);
+        cacheSize = items.cacheSize;
     });
 }
 
 // Jump to suggested URL
 function goToPage(hash) {
-    chrome.tabs.update({
-        "url": gateway + hash
+    let jumpGateway = gateway;
+    if (jumpGateway[jumpGateway.length - 1] != "/") {
+        jumpGateway += "/";
+    }
+    let jumpURL = jumpGateway + hash;
+    chrome.tabs.update(null, {
+        "url": jumpURL
     }, function () {
-        console.log("Bug Page is open");
+        console.log("Executing jump to " + jumpURL);
     });
 }
 
 // Search on IPFS (default)
 function standardSearch(text) {
+    let searchURL = "https://ipfs-search.com/#/search?search=" + text;
     chrome.tabs.update({
-        "url": "https://ipfs-search.com/#/search?search=" + text
+        "url": searchURL
     }, function () {
-        console.log("Bug Page is open");
+        console.log("Executing search: " + searchURL);
     });
 
 }
 
 // Query the IPFS search API
 function querySearch(text) {
-    fetch("https://api.ipfs-search.com/v1/search?q=" + encodeURI(text) + "&type=any&page=1", {
-            mode: 'cors'
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            parseSuggestions(myJson, text);
-        })
-        .catch(function (error) {
-            console.log("Error: " + error);
-        });
+    let cachedSuggestion = getCachedSuggestion(text);
+    if (cachedSuggestion == null) {
+        fetch("https://api.ipfs-search.com/v1/search?q=" + encodeURI(text) + "&type=any&page=1", {
+                mode: 'cors'
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                parseSuggestions(myJson, text);
+            })
+            .catch(function (error) {
+                console.log("Error: " + error);
+            });
+    } else {
+        setDefaultSuggestion(cachedSuggestion.title, text);
+    }
+}
+
+function getCachedSuggestion(text) {
+    text = text.trim();
+
+    let result = storedSuggestions.find((el) => el.key == text);
+    if (result == undefined) {
+        return null;
+    }
+    return result.value;
+}
+
+function setDefaultSuggestion(title, originalText) {
+    let trailingText = getTrailingText(originalText);
+
+    // Chrome only allows one suggestion from an extension
+    chrome.omnibox.setDefaultSuggestion({
+        description: (title + trailingText)
+    });
 }
 
 // An action is treated as non-default if its query ends with a space
@@ -64,17 +98,18 @@ function getTrailingText(text) {
     let trailingText = "<dim> | ";
 
     let useDefault = isDefault(text);
+    let cachedSuggestion = getCachedSuggestion(text);
 
     if (defaultAction === "go") {
-        if (useDefault) {
-            trailingText += "Jump to Page (Space + Enter to search)";
-        } else {
+        if (!useDefault || cachedSuggestion == null) {
             trailingText += "Search on IPFS";
+        } else {
+            trailingText += "Jump to Page (Space + Enter to search)";
         }
     } else {
         // Includes both "search" and any unrecognized value
 
-        if (useDefault) {
+        if (useDefault || cachedSuggestion == null) {
             trailingText += "Search on IPFS (Space + Enter to jump to page)";
         } else {
             trailingText += "Jump to Page";
@@ -95,14 +130,19 @@ function parseSuggestions(jsonResponse, originalText) {
     // Set first suggestion as the default suggestion
     console.log("Received suggestion response: " + jsonResponse);
     let hit = jsonResponse.hits[0];
-    storedSuggestion = hit.hash;
 
-    let trailingText = getTrailingText(originalText);
-
-    // Chrome only allows one suggestion from an extension
-    chrome.omnibox.setDefaultSuggestion({
-        description: (hit.title + trailingText)
+    if (storedSuggestions.length > cacheSize) {
+        storedSuggestions.shift();
+    }
+    storedSuggestions.push({
+        key: originalText,
+        value: {
+            title: hit.title,
+            hash: hit.hash
+        }
     });
+
+    setDefaultSuggestion(hit.title, originalText);
 
 }
 
@@ -117,13 +157,15 @@ chrome.omnibox.onInputChanged.addListener(
 // Fired when the user accepts the suggestion
 chrome.omnibox.onInputEntered.addListener(
     function (text) {
-        if (storedSuggestion == null) {
-            // In case of error, fallback to search
+        let cachedSuggestion = getCachedSuggestion(text);
+        if (cachedSuggestion == null) {
+            // In case of error, fall back to search
             standardSearch(text);
+            return;
         }
         if (isDefault(text)) {
             if (defaultAction === "go") {
-                goToPage(storedSuggestion, text);
+                goToPage(cachedSuggestion.hash, text);
             } else {
                 standardSearch(text);
             }
@@ -132,7 +174,7 @@ chrome.omnibox.onInputEntered.addListener(
             if (defaultAction === "go") {
                 standardSearch(text);
             } else {
-                goToPage(storedSuggestion, text);
+                goToPage(cachedSuggestion.hash, text);
             }
         }
     });
--- options.js
+++ options.js
@@ -1,31 +1,36 @@
 // Save options to chrome.storage
 function save_options() {
-    let defaultAction = document.getElementById("defaultAction").value;
-    let gateway = document.getElementById("gateway").value;
-    chrome.storage.sync.set({
+  let defaultAction = document.getElementById("defaultAction").value;
+  let gateway = document.getElementById("gateway").value;
+  let cacheSize = document.getElementById("cacheSize").value;
+  chrome.storage.sync.set({
       defaultAction: defaultAction,
-      gateway: gateway
-    }, function() {
+      gateway: gateway,
+      cacheSize: cacheSize
+  }, function () {
       // Update status to let user know options were saved.
       let status = document.getElementById("status");
+      status.style.opacity = "100%";
       status.textContent = "Options saved.";
-      setTimeout(function() {
-        status.textContent = "";
+      setTimeout(function () {
+          status.style.opacity = "0%";
       }, 750);
-    });
-  }
-  
-// Restore select box and checkbox state using the preferences
-// stored in chrome.storage.
+  });
+}
+
+// Restore the state using the preferences
+// stored in chrome.storage
 function restore_options() {
   chrome.storage.sync.get({
-    defaultAction: "search",
-    gateway: "https://gateway.ipfs.io/ipfs/"
-  }, function(items) {
-    console.log("Setting gateway to " + items.gateway)
-    document.getElementById("gateway").value = items.gateway;
+      defaultAction: "search",
+      gateway: "https://gateway.ipfs.io/ipfs/",
+      cacheSize: 100
+  }, function (items) {
+      document.getElementById("defaultAction").value = items.defaultAction;
+      document.getElementById("gateway").value = items.gateway;
+      document.getElementById("cacheSize").value = items.cacheSize;
   });
 }
 document.addEventListener("DOMContentLoaded", restore_options);
 document.getElementById("save").addEventListener("click",
-    save_options);
+  save_options);
